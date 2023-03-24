cdfefa4b17b4bdf0d39f99a05a56bd382ce58b2d
LioraOceacovschi <liora.skliar@gmail.com>
2023-03-24 04:02:23 +0300

    changes

---
+++ src/test/java/StartSelenium.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class StartSelenium {
+    WebDriver wd;
+    @BeforeMethod
+    public void preCondition(){{
+
+    }
+
+    }
+
+    @Test
+    public void testName(){
+
+    }
+
+    @AfterMethod
+    public void postCondition(){
+
+
+    }
+
+}
